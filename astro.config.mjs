64f66d449aea39ad1e9a0d595c1de89033226553
Juan Manuel Panozzo Zenere <juanmanuel.panozzozenere@alumnos.uai.edu.ar>
2025-05-12 19:33:03 -0300

    Add Tailwind CSS and React integration with global styles and hero component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,26 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig,  fontProviders} from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import react from '@astrojs/react';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    vite: {
+        plugins: [tailwindcss()]
+    },
+    experimental: {
+        fonts: [{
+            provider: fontProviders.google(),
+            name: "Manrope",
+            cssVariable: "--font-manrope"
+        },
+        {
+            provider: fontProviders.google(),
+            name: "Caveat",
+            cssVariable: "--font-caveat"
+        }]
+    },
+    integrations: [react()]
+});
